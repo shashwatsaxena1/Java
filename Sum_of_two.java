8562fa11b76a4e41123142cce40daa6c38ad5426
Shashwat <shashwatsaxena17@gmail.com>
2026-06-07 22:06:33 +0530

    basic maths

---
+++ Sum_of_two.java
@@ -0,0 +1,8 @@
+public class Sum_of_two {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
