17474abd5b7c8659305c112a977abeb771513b61
Shashwat <shashwatsaxena17@gmail.com>
2026-06-08 23:10:58 +0530

    Using Variable

---
+++ Bacis/SwapUsingThird.java
@@ -0,0 +1,18 @@
+package Bacis;
+import java.util.Scanner;
+public class SwapUsingThird {
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("After swapping:");
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+        sc.close();
+    }
+}
