e6f1ac2800f57a7b9aa4fae09c00bfa203ec8297
Shashwat <shashwatsaxena17@gmail.com>
2026-06-08 23:27:22 +0530

    logic

---
+++ Basic/SwapWithoutThird.java
@@ -0,0 +1,18 @@
+package Basic;
+import java.util.Scanner;
+public class SwapWithoutThird {
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("After swapping:");
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+        sc.close();
+    }
+}
