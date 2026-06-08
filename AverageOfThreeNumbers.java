53c1a86b94185b0a3de01a32195ffd79d4804266
Shashwat <shashwatsaxena17@gmail.com>
2026-06-08 15:49:11 +0530

    basic

---
+++ AverageOfThreeNumbers.java
@@ -0,0 +1,11 @@
+public class AverageOfThreeNumbers {
+    public static void main(String[] agrs){
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        double average = (num1 + num2 + num3) / 3.0;
+
+        System.out.println("The average of " + num1 + ", " + num2 + ", and " + num3 + " is: " + average);
+    }    
+}
---
+++ ProductOfTwoNumbers.java
@@ -0,0 +1,10 @@
+public class ProductOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5; 
+        int num2 = 10; 
+
+        int product = num1 * num2;
+
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
