dd2d6c33999c26ff533abbe10dc53fcb358b5584
Shashwat <shashwatsaxena17@gmail.com>
2026-06-08 17:19:55 +0530

    Maths

---
+++ Maths/SubtractTwo.java
@@ -0,0 +1,12 @@
+package Maths;
+
+public class SubtractTwo {
+    public static void main(String[] args) {
+        int num1 = 15; 
+        int num2 = 5; 
+
+        int difference = num1 - num2;
+
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+    }
+}
