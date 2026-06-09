c2b604460e2b5f57dc083ba9af9a3be9dc887912
Shashwat <shashwatsaxena17@gmail.com>
2026-06-09 23:59:27 +0530

    Geometry

---
+++ Maths/AreaTriangle.java
@@ -0,0 +1,14 @@
+package Maths;
+import java.util.Scanner;
+public class AreaTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base of the triangle: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter the height of the triangle: ");
+        double height = sc.nextDouble();
+        double area = 0.5 * base * height;
+        System.out.println("The area of the triangle is: " + area);
+        sc.close();
+    }
+}
---
+++ Maths/PerimeterRectangle.java
@@ -0,0 +1,14 @@
+package Maths;
+import java.util.Scanner;
+public class PerimeterRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle: ");
+        double length = sc.nextDouble();
+        System.out.print("Enter the width of the rectangle: ");
+        double width = sc.nextDouble();
+        double perimeter = 2 * (length + width);
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+        sc.close();
+    }
+}
