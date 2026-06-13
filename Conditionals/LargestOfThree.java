ae719ea689f9c8acf576a95fa9c8dd0bb000d5f1
Shashwat <shashwatsaxena17@gmail.com>
2026-06-13 22:06:57 +0530

    Largest Number

---
+++ Conditionals/LargestOfThree.java
@@ -0,0 +1,21 @@
+package Conditionals;
+import java.util.*;
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
+        if(num1 >= num2 && num1 >= num3){
+            System.out.println(num1 + " is the largest number.");
+        } else if(num2 >= num1 && num2 >= num3){
+            System.out.println(num2 + " is the largest number.");
+        } else {
+            System.out.println(num3 + " is the largest number.");
+        }
+        sc.close();
+    }
+}
