27aa330432b3753e7a88a7fdd0a6df561db32aed
Shashwat <shashwatsaxena17@gmail.com>
2026-06-13 22:03:50 +0530

    number system

---
+++ Conditionals/PositiveNegativeZero.java
@@ -0,0 +1,17 @@
+package Conditionals;
+import java.util.*;
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if(num > 0){
+            System.out.println("The number is positive.");
+        } else if(num < 0){
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        sc.close();
+    }
+}
