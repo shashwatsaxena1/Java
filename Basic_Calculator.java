51aa1126dbdfdd23b854c42646df4c0533671106
Shashwat <shashwatsaxena17@gmail.com>
2026-06-21 00:03:55 +0530

    Calculator

---
+++ Basic_Calculator.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Basic_Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    sc.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                sc.close();
+                return;
+        }
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
