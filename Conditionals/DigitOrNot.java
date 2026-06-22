e27e4abcbbbacce56183667720cef34aaf6f33f6
Shashwat <shashwatsaxena17@gmail.com>
2026-06-23 00:00:18 +0530

    Num Or Not

---
+++ Conditionals/DigitOrNot.java
@@ -0,0 +1,15 @@
+package Conditionals;
+import java.util.Scanner;
+public class DigitOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        if (Character.isDigit(ch)) {
+            System.out.println(ch + " is a digit.");
+        } else {
+            System.out.println(ch + " is not a digit.");
+        }
+        sc.close();
+    }
+}
