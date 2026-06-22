0f41ebf6ae0628c6d96f4e8823a5761f87483330
Shashwat <shashwatsaxena17@gmail.com>
2026-06-22 23:54:52 +0530

    Alpha

---
+++ Conditionals/AlphaOrNot.java
@@ -0,0 +1,15 @@
+package Conditionals;
+import java.util.Scanner;
+public class AlphaOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        if (Character.isLetter(ch)) {
+            System.out.println(ch + " is an alphabet.");
+        } else {
+            System.out.println(ch + " is not an alphabet.");
+        }
+        sc.close();
+    }
+}
