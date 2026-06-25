e48f768521e517feff9a9dd8813e65926e6c8410
Shashwat <shashwatsaxena17@gmail.com>
2026-06-25 23:29:09 +0530

    Upper To Lower & Vice Versa

---
+++ Conditionals/CaseChnager.java
@@ -0,0 +1,14 @@
+// upper case to lower case and vice versa
+
+package Conditionals;
+import java.util.Scanner;
+public class CaseChnager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        System.out.println("Original string: " + input);
+        System.out.println("Uppercase: " + input.toUpperCase());
+        System.out.println("Lowercase: " + input.toLowerCase());
+    }
+}
